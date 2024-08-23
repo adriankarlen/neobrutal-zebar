fac1d869157f5278fe50bd3606434fbd040bd1a8
adriankarlen <adrian.karlen@spp.se>
2024-08-23 13:42:17 +0200

    feat!: inital commit

---
+++ scripts/glazewm.js
@@ -0,0 +1,63 @@
+const processIconMap = {
+  // Terminals
+  "wezterm-gui": { icon: "ti-terminal-2" },
+  alacritty: { icon: "ti-terminal-2" },
+  WindowsTerminal: { icon: "ti-terminal-2" },
+
+  // Editors
+  code: { icon: "ti-brand-vscode" },
+  devenv: { icon: "ti-brand-visual-studio" },
+
+  // Communication
+  "ms-teams": { icon: "ti-brand-teams" },
+  olk: { icon: "ti-mail" },
+  "application-frame-host": { icon: "ti-spy" },
+
+  // Ignore
+  msedgewebview2: { ignore: true },
+};
+
+export const focusWorkspace = (event, context) => {
+  console.log("Focus button clicked!", event, context);
+  const id = event.target.id;
+  context.providers.glazewm.focusWorkspace(id);
+};
+
+const addProcessIconCallback = (mutationsList) => {
+  for (const mutation of mutationsList) {
+    if (mutation.type === "childList") {
+      const node = document.querySelector("#process-container");
+      if (node) {
+        const iconNodes = node.querySelectorAll(".ti");
+
+        iconNodes.forEach((iconNode) => {
+          const processName = iconNode.getAttribute("data-process-name");
+
+          if (!processName) return;
+
+          const process = processIconMap[processName];
+
+          const unmapped = process == null;
+          if (!unmapped && process.ignore) {
+            iconNode.remove();
+            return;
+          }
+
+          iconNode.classList.add(
+            unmapped ? `ti-brand-${processName}` : process.icon,
+          );
+        });
+      }
+    }
+  }
+};
+
+const observer = new MutationObserver(addProcessIconCallback);
+const config = { childList: true, subtree: false };
+const parentNode = document.querySelector("#glazewm-workspaces");
+if (parentNode) {
+  observer.observe(parentNode, config);
+} else {
+  console.error("Parent node #glazewm-workspaces not found.");
+}
+;
---
+++ scripts/menu.js
@@ -0,0 +1,19 @@
+export function toggleDropdown() {
+  const menu = document.querySelector("#dropdown");
+  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+}
+
+export function powerDown() {
+  const menu = document.querySelector("#dropdown");
+  menu.style.display = "none";
+}
+
+export function sleep() {
+  const menu = document.querySelector("#dropdown");
+  menu.style.display = "none";
+}
+
+export function logout() {
+  const menu = document.querySelector("#dropdown");
+  menu.style.display = "none";
+}
