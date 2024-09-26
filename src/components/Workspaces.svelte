32d62a6287f470ec65cddec6950167227e3cab59
adriankarlen <adrian.karlen@spp.se>
2024-09-26 16:20:14 +0200

    fix(glazewm): better structure for icons

--- src/components/Workspaces.svelte
+++ src/components/Workspaces.svelte
@@ -4,63 +4,27 @@
   import type { Window } from "glazewm";
   import { onMount } from "svelte";
   import type { GlazeWmOutput } from "../types/providers";
-
-  type Icon = {
-    icon?: string;
-    ignore?: boolean;
-  };
-  type IconMap = {
-    [key: string]: Icon;
-  };
-
-  const iconMap = {
-    // Terminals
-    "wezterm-gui": { icon: "ti-terminal-2" },
-    alacritty: { icon: "ti-terminal-2" },
-    windowsterminal: { icon: "ti-terminal-2" },
-
-    // Editors
-    code: { icon: "ti-brand-vscode" }, // VS Code
-    devenv: { icon: "ti-brand-visual-studio" }, // Visual Studio
-
-    // Communication
-    "ms-teams": { icon: "ti-brand-teams" },
-    olk: { icon: "ti-mail" }, // Outlook
-
-    // VPN
-    "azure vpn client": { icon: "ti-spy" }, // Azure VPN Client
-
-    // Browsers
-    zen: { icon: "ti-circle-letter-z" },
-    msedge: { icon: "ti-brand-edge" },
-
-    // Utils
-    snippingtool: { icon: "ti-screenshot" },
-    "control panel": { icon: "ti-settings" },
-    explorer: { icon: "ti-folder" },
-    photos: { icon: "ti-photo" },
-    sound: { icon: "ti-headphones" },
-    excel: { icon: "ti-file-spreadsheet" },
-    onenote: { icon: "ti-note" },
-    powerpnt: { icon: "ti-presentation" },
-    winword: { icon: "ti-file-word" },
-    mspaint: { icon: "ti-palette" },
-
-    // Ignore
-    msedgewebview2: { ignore: true }
-  } as IconMap;
+  import iconMap from "$lib/icon_map.json";
+  import { get } from "svelte/store";
 
   const getProcessIcon = (child: Window) => {
-    const processEntry = iconMap[child.processName.toLowerCase()];
-    const titleEntry = iconMap[child.title];
+    if (!child) return "";
 
-    if (processEntry && !processEntry.ignore) {
-      return processEntry.icon;
-    } else if (titleEntry && !titleEntry.ignore) {
-      return titleEntry.icon;
-    } else {
-      return "ti-brand-" + child.processName.toLowerCase(); //
-    }
+    let entry = iconMap.find((entry) =>
+      entry.appNames
+        .map((name) => name.toLowerCase())
+        .includes(child.processName.toLowerCase())
+    );
+
+    if (!entry)
+      entry = iconMap.find((entry) =>
+        entry.appNames
+          .map((name) => name.toLowerCase())
+          .includes(child.title.toLowerCase())
+      );
+    return entry
+      ? (entry!.iconName ?? "")
+      : `ti-brand-${child.processName.toLowerCase()}`;
   };
 
   let glazewmOutput = $state<GlazeWmOutput>();
@@ -72,7 +36,7 @@
 </script>
 
 {#if glazewmOutput}
-  <div class="flex flex-row gap-2">
+  <div class="flex flex-row gap-2 items-center">
     {#each glazewmOutput.currentWorkspaces as workspace, i}
       <Button
         iconClass="ti {workspace.hasFocus ? 'ti-point-filled' : 'ti-point'}"
@@ -87,17 +51,20 @@
     >
       <i class="ti ti-switch-{glazewmOutput?.tilingDirection}"></i>
     </button>
-    <div class="flex items-center gap-2">
+    <div class="flex items-center gap-1">
       {#if glazewmOutput.focusedWorkspace}
         {#each glazewmOutput.focusedWorkspace!.children as child}
           {#if "state" in child && child.state?.type != "minimized"}
-            <span
-              class={child.hasFocus
-                ? "text-zb-focused-process"
-                : "text-zb-process"}
-            >
-              <i class="ti {getProcessIcon(child as Window)}"></i>
-            </span>
+            {@const icon = getProcessIcon(child as Window)}
+            {#if icon}
+              <span
+                class={child.hasFocus
+                  ? "text-zb-focused-process"
+                  : "text-zb-process"}
+              >
+                <i class="ti {icon}"></i>
+              </span>
+            {/if}
           {/if}
         {/each}
       {/if}
